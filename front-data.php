0f584ce03120bf839e4197faa1be68e62c5c2d8e
adialmuhsin <adialmuhsin@gmail.com>
2020-01-23 10:33:20 +0700

    update template

--- front-data.php
+++
@@ -1,4 +0,0 @@
-<?php
-global $complete;
-?>
-<?php include( get_page_template() ); ?>
--- frontpage/slider-customslider.php
+++
@@ -1,6 +0,0 @@
-<?php global $complete; ?>
-<?php if(!empty($complete['custom_slider']) || is_customize_preview() ) { ?>
-<div class="slidercus">
-<?php echo do_shortcode($complete['custom_slider']); ?>
-</div> 
-<?php } ?>
